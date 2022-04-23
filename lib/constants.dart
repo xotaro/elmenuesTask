0ad0fc50d2333605a2b1e95bed4a860cb469fbb3
xotaro <47543222+xotaro@users.noreply.github.com>
2022-04-23 19:24:21 +0200

    Delete constants.dart

--- lib/constants.dart
+++
@@ -1,6 +0,0 @@
-const AUTH0_DOMAIN = String.fromEnvironment('AUTH0_DOMAIN');
-const AUTH0_CLIENT_ID = String.fromEnvironment('AUTH0_CLIENT_ID');
-const AUTH0_ISSUER = 'https://$AUTH0_DOMAIN';
-const BUNDLE_IDENTIFIER = 'com.example.elmenues';
-const AUTH0_REDIRECT_URI = '$BUNDLE_IDENTIFIER://login-callback';
-const REFRESH_TOKEN_KEY = 'refresh_token';
